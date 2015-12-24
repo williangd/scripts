12f1ee0b4690103944aa1961532a4dd451148dbf
Willian <williangd@yahoo.com.br>
2015-12-24 09:02:56 -0200

    Alterado comandos

--- ubuntu.py
+++ ubuntu.py
@@ -1,39 +1,35 @@
 # -*- coding: utf-8 -*-
 import subprocess, os
 
-def upgrade(debug=False):
-    cmds= """
-        sudo apt-get update
-        sudo apt-get dist-upgrade
-    """.strip().splitlines()
-
-    for cmd in cmds:
+def upgrade():
+    cmds= [ 'sudo apt-get update', 'sudo apt-get dist-upgrade' ]
+    for cmd in cmds: 
+        print(cmd)
         os.system(cmd)
 
 def java():
-    cmds="""
-        sudo add-apt-repository ppa:webupd8team/java -y
-        sudo apt-get update
-        sudo apt-get install oracle-java8-installer oracle-java8-set-default -y
-    """.strip().splitlines()
-
+    cmds= [ 'sudo add-apt-repository ppa:webupd8team/java -y', 'sudo apt-get update', 'sudo apt-get install oracle-java8-installer oracle-java8-set-default -y' ]
     for cmd in cmds:
         os.system(cmd)
+
+
+def pacotes()
+    os.system(sudo apt-get install bzip2 curl dia filezilla gimp gip gzip lynx nmap openssh-server openvpn nload rar rsync sshfs traceroute trickle unrar unzip wget wireshark zip fping mtr-tiny whois)
 
 def menu():
     print("Opções:")
     print('1 - Atualizar')
     print('2 - Java')
-    print('3 - Sair')
+    print('99 - Sair')
 
 
 def opcoes():
-    op = int(input('Entre com a opção: '))
-    if op == 1:
+    op = input('Entre com a opção: ')
+    if op == '1':
         upgrade()
-    elif op == 2:
+    elif op == '2':
         java()
-    elif op == 3:
+    elif op == '99':
         print('Saindo..')
     else:
         print('Opção inválida!')
